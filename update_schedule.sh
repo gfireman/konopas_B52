661b2899d39cd6cfa7bf28b5b523d2e19af89686
Gwyn Fireman <gfireman@gmail.com>
2021-05-25 22:59:14 -0400

    B55v2 - more links

--- update_schedule.sh
+++ update_schedule.sh
@@ -2,16 +2,9 @@
 
 # setups
 kondir=$(dirname "${BASH_SOURCE[0]}")
-jsonfile=balticon55.json
-manifest=konopas.appcache
-timestamp=$(date "+%Y%m%d_%H%M")
-
-# save current state of schedule
-oldfile=$kondir/data/$jsonfile
-newfile=$kondir/data/archive/$timestamp.$jsonfile
-mkdir -p $(dirname $newfile)
-rsync -a $oldfile $newfile
+jsonfile=$kondir/data/balticon55.json
+manifest=$kondir/konopas.appcache
 
 # update the appcache
-timestamp=$(TZ='America/New_York' date)
-sed -i "s/^# .*/# $timestamp/" $kondir/$manifest 2>/dev/null
+timestamp=$(TZ='America/New_York' date -r $jsonfile)
+sed -i "s/^# .*/# $timestamp/" $manifest 2>/dev/null
